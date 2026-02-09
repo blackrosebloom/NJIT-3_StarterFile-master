98243a9f8d8a295a571aad6c2f9be9123e075c77
blackrosebloom <226smuller@frhsd.com>
2026-02-09 09:23:31 -0500

    step 3

--- site.js
+++ site.js
@@ -34,7 +34,9 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
+         title: 'IMDB + Sofia\'s movie/tv show list',
+         owner:'sofia',
+         url:'',
       }
     },
       methods: {
